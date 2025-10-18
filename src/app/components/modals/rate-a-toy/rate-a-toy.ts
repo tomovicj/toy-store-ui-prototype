7a2a2802884b78107c2895860b7949398786622f
Tomovic <jovantomovic5@gmail.com>
2025-10-18 14:41:30 +0200

    Implement form in rate-a-toy modal

--- src/app/components/modals/rate-a-toy/rate-a-toy.ts
+++ src/app/components/modals/rate-a-toy/rate-a-toy.ts
@@ -1,11 +1,19 @@
-import { AfterViewInit, Component, signal, ViewChild } from '@angular/core';
-import { Toy } from '../../../models/toy';
-import { Modal } from 'bootstrap';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rate-a-toy',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './rate-a-toy.html',
   styleUrl: './rate-a-toy.css',
 })
-export class RateAToy {}
+export class RateAToy {
+  form = new FormGroup({
+    rating: new FormControl('', [Validators.required, Validators.min(1), Validators.max(5)]),
+    review: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    // TODO: Implement submission logic
+  }
+}
